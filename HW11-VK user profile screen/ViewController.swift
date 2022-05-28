32304b9c8bfdc20320c4be127bf0e641f5d43dd7
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-05-28 14:10:17 +0300

    Initial Commit

---
+++ HW11-VK user profile screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW11-VK user profile screen
+//
+//  Created by Anatoliy on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
